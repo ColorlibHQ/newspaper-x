cf186bde9f5dfb4657928d27baee937c9bf008a0
briciag <sabin@veho.ro>
2017-05-19 13:11:53 +0300

    Newspaper X  - Search form

--- searchform.php
+++ searchform.php
@@ -1,8 +1,11 @@
-<form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<?php $has_menu = has_nav_menu( 'social' ); ?>
+<form role="search" method="get"  <?php echo $has_menu ? '' : 'class="pull-right"' ?> id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo esc_html__( 'Search for:', 'newspaper-x' ) ?></span>
-		<input class="search-field" placeholder="<?php echo esc_html__( 'Search ...', 'newspaper-x' ) ?>" value="" name="s"
+		<input class="search-field" placeholder="<?php echo esc_html__( 'Search...', 'newspaper-x' ) ?>" value="" name="s"
 		       type="search">
 	</label>
 	<button class="search-submit" value="<?php echo esc_html__( 'Search  ', 'newspaper-x' ) ?>" type="submit"><span class="fa fa-search"></span></button>
 </form>
+
+
